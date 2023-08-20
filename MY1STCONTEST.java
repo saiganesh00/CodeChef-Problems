71595649b1c4a4a271ee8a131270245dc47cd305
Shanmukh Maddimsetti <140135125+saiganesh00@users.noreply.github.com>
2023-08-20 17:56:45 +0530

    Day-27

---
+++ MY1STCONTEST.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print((n - a) + " ");
+		System.out.print(n - (a+b));
+	}
+}
