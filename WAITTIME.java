f0d013e8f1d2cebb670d62cc22e9ef2c16adb117
Shanmukh Maddimsetti <140135125+saiganesh00@users.noreply.github.com>
2023-08-28 22:31:28 +0530

    Day-35

---
+++ WAITTIME.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int K = sc.nextInt();
+		    int X = sc.nextInt();
+		    int res = (K * 7) - X;
+		    System.out.println(res);
+		}
+	}
+}
